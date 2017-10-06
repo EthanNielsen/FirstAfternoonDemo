c2b8d0d3241736702780ab5bb1e8260d5c89ff1b
EthanNielsen <31518851+EthanNielsen@users.noreply.github.com>
2017-10-06 13:10:36 -0600

    when I click my button I now change colors between orange and black.

--- FirstAfternoonDemo/ViewController.swift
+++ FirstAfternoonDemo/ViewController.swift
@@ -12,6 +12,17 @@ class ViewController: UIViewController
 {
     @IBOutlet weak var firstButton: UIButton!
    
+    @IBAction func firstMethod(_ sender: UIButton)
+    {
+        if ( firstButton.backgroundColor == .orange)
+        {
+            firstButton.backgroundColor = .black
+        }
+        else
+        {
+            firstButton.backgroundColor = .orange
+        }
+    }
     override func viewDidLoad()
     {
         super.viewDidLoad()
